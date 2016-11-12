bcfebbdb1e756def47c91481cd8fca4e19f7d966
Raghurammandapally <mandapallyraghuram@gmail.com>
2016-11-12 20:30:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/page1.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
+app.get('/ui/about.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'about.html'));
 });
 
 
